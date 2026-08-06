254f8ec2d490a4393533883d70ab2e308200ba59
Kazi Ishmamul Haque <kaziishmamulhaque@gmail.com>
2026-08-06 13:34:38 +0600

    feat(pricing): add WeatherForecastGrid entity and repository

---
+++ src/main/java/com/turfchai/pricing/repository/WeatherForecastGridRepository.java
@@ -0,0 +1,10 @@
+package com.turfchai.pricing.repository;
+
+import com.turfchai.pricing.entity.WeatherForecastGrid;
+import com.turfchai.pricing.entity.WeatherForecastGridId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeatherForecastGridRepository extends JpaRepository<WeatherForecastGrid, WeatherForecastGridId> {
+}
